9fdd1f2a6d708886627ab19cf2fc4b86f4bb88de
prestij1987 <lehaleyba@mail.ru>
2024-02-29 17:05:48 +0300

    git commit  -m 'change age'

--- tryecept.py
+++ tryecept.py
@@ -1,8 +1,9 @@
 vozr= input('Ваш возраст; ')
 age = int(vozr)
 res = 0
+year =2024
 
-while age > 0:
+while age > 20 and age < 100:
     res = age + 1
     if res == 10:
         print('Цикл закончен')
