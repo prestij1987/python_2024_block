426cb65b4b693bdd45947e2f97cd025b6a70c66c
prestij1987 <lehaleyba@mail.ru>
2024-02-29 20:46:43 +0300

    Проверяем ошибку данных

--- tryecept.py
+++ tryecept.py
@@ -1,4 +1,4 @@
-vozr= input('Ваш возраст; ')
+'''vozr= input('Ваш возраст; ')
 age = int(vozr)
 res = 0
 year =2024
@@ -12,7 +12,22 @@ while age > 20 and age < 100:
             print('Введите ваш возраст')
     except ZeroDivisionError:
        ## if age < 0 and age < 100:
-            print('Возраст отрицательный ') 
+            print('Возраст отрицательный ')
     except NameError:
-            print('Ошибка данных') 
+            print('Ошибка данных')'''
 
+'''birth_year = int(input('Какой твой год рождения?'))
+if birth_year < 1800:
+    raise ValueError(
+        'Ты - исторический персонаж? Столько не живут')'''
+
+# Исправьте программу, запрашивающую год рождения с добавлением собственного исключения
+
+born = input('Введите ваш год рождения: ')
+born_1 =  2024 - int(born)
+try:
+    if born_1 < 80 and born_1 < int(-1):
+        raise ValueError('Ты не родился ')
+    print('Попробуй еще: ')
+except ZeroDivisionError:
+    print('Ошибка произошла ')
