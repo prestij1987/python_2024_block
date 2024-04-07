4eb9b0bf8b2b68af011ab3d3dfa0ded6c900ab92
prestij1987 <94439314+prestij1987@users.noreply.github.com>
2024-04-07 10:41:29 +0300

    Update pole_chudes.py

--- module/pole_chudes.py
+++ module/pole_chudes.py
@@ -1,19 +1,22 @@
 import random
 #Создать игру поле чудес
-import dict_word
+from dict_world import zagadka
 
-# slovo = ['колесо', 'замок', 'золото', 'тарелка']
-
-#zapros = input('Загадай одно из слов', slovo)
-#zapros1 = random.choice(list(dict_word)) cлучайное слово из словаря
-zapros1 = list(dict_word)
+zapros1 = list(zagadka)
 otvet = ''
 
 while zapros1 != otvet:
     otvet =+ input( f'введите букву: ')
-    if zapros1 == otvet:
-        hide_word = '_' * len(zapros1) 
-        print('Вы отгадали слово', hide_word)    
+    for i in zapros1:
+        for it in otvet:
+
+            if zapros1[i] == otvet[it]:
+                print('Печатаем букву ')
+            else:
+                print('Ввели неправильное букву ппробуйте еще раз ')    
+          
+                hide_word = '_' * len(zapros1) 
+    print('Вы отгадали слово', hide_word)    
      
 
 
